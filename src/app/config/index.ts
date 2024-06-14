be98d208a2b4cb1ae9e6374b76a5e66836c7341b
Jubair <jubair.ahmed2838@gmail.com>
2024-06-14 15:41:41 +0600

    [Add]: jwt access token create func

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,4 +8,5 @@ export default {
   database_url: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,6 +2,9 @@ import httpStatus from 'http-status';
 import { AppError } from '../../errors/AppError';
 import { UserModel } from '../user/user.model';
 import { ILoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 const loginUser = async (payload: ILoginUser) => {
   // checking if the user exist
@@ -10,6 +13,32 @@ const loginUser = async (payload: ILoginUser) => {
   if (!isUserExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'User not find');
   }
+
+  // check password
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    isUserExists.password,
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Password does not match');
+  }
+
+  // Access Token, refresh token
+
+  // create token and send to client
+  const jwtPayload = {
+    userEmail: isUserExists?.email,
+    userRole: isUserExists?.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+
+  return {
+    accessToken,
+  };
 };
 
 export const AuthServices = {
